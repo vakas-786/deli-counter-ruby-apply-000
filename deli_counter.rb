c886ba8ceaaa3498bacb100456e64d0467ab4401
Vakas  Akhtar <vakhtar@fordham.edu>
2020-04-22 15:47:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,10 +17,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
-  #shovel method to put customer at end of the line
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  #.length returns number of elements in array
+  
 end
 
 def now_serving(katz_deli)
